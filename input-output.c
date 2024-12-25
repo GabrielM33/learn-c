3b4b64c00ff1d8559aaed307635832c9b718a04b
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-25 12:25:39 -0300

    add all files

---
+++ input-output.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // Output
+    int number = 42;
+    printf("The number is: %d\n", number);
+    printf("Float: %.2f\n", 3.14159);
+
+    // Input
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    return 0;
+}
